764abefb5f2527c54f5131ef31ad421d74523451
RodolfoBlanco <www.rodolfoblanco@hotmail.com>
2020-08-06 13:54:44 -0500

    @Rodolfo Blanco

--- TalentoIT/TalentoIT/Startup.cs
+++ TalentoIT/TalentoIT/Startup.cs
@@ -63,7 +63,7 @@ namespace TalentoIT
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Reclutador}/{action=Index}/{id?}");
             });
         }
     }
